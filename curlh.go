79a4b7fd3e69a6ef02953c91c405a28dd3048451
nirwander <nirwander@gmail.com>
2018-07-06 23:58:55 +1000

    1

--- curlh.go
+++ curlh.go
@@ -4,15 +4,17 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"os/exec"
 	"strings"
 	"time"
 )
 
-const curl = `D:\_Soft\curl-7.60.0-win64-mingw\bin\curl.exe`
+//const curl = `D:\_Soft\curl-7.60.0-win64-mingw\bin\curl.exe`
+const curl = `curl.exe`
 
 func main() {
 	//Получаем список файлов
-	fileBytes, err := ioutil.ReadFile(`E:\list.txt`)
+	fileBytes, err := ioutil.ReadFile(`list.txt`)
 	if err != nil {
 		panic(err)
 	}
@@ -22,15 +24,15 @@ func main() {
 	result := make(chan string, 10)
 	limit := make(chan int, 3)
 
-	for j, line := range lines {
-		fmt.Println(j)
-		fmt.Println(time.Now().Format(time.RFC3339))
+	for _, line := range lines {
+		//fmt.Println(j)
+		//fmt.Println(time.Now().Format(time.RFC3339))
 		limit <- 1
-		go runCurl(line, result, limit)
+		go runCurl(string(line), result, limit)
 	}
 
-	fmt.Println("Waiting routines")
-	for i := 0; i < len(lines); i++ {
+	//fmt.Println("Waiting routines")
+	for range lines {
 		fmt.Println(<-result)
 	}
 	fmt.Println("Done")
@@ -41,8 +43,8 @@ func main() {
 func runCurl(file string, res chan string, limit chan int) {
 	file = strings.TrimSpace(file)
 	//fmt.Println(args)
-	//pos := strings.Index(file, "SR 3-")
-	//sr := file[pos+3 : pos+16]
+	pos := strings.Index(file, "SR 3-")
+	sr := file[pos+3 : pos+16]
 	//fmt.Println(pos)
 	//fmt.Println(sr)
 
@@ -53,26 +55,28 @@ func runCurl(file string, res chan string, limit chan int) {
 	cmdArgs = append(cmdArgs, `ivan.zotov@megafon.ru:.Member3`)
 	cmdArgs = append(cmdArgs, `--proxy`)
 	cmdArgs = append(cmdArgs, `http://dv-proxy.megafon.ru:3128`)
-	//cmdArgs = append(cmdArgs, `https://transport.oracle.com/upload/issue/`+sr+`/`)
+	cmdArgs = append(cmdArgs, `https://transport.oracle.com/upload/issue/`+sr+`/`)
 
 	//fmt.Println(cmdArgs)
 	start := time.Now()
 	// Делаем 5 попыток загрузки
-	/*for i := 0; i < 5; i++ {
+	var uploaded bool
+	for i := 0; i < 5; i++ {
 		cmd := exec.Command(curl, cmdArgs...)
-		out, err := cmd.CombinedOutput()
+		err := cmd.Run()
 		if err == nil {
+			uploaded = true
 			break
 		}
-		fmt.Printf("Try #%d ", i)
+		fmt.Printf("Try No. %d", i)
 		fmt.Println(err)
-		fmt.Println(string(out))
-	}*/
-	time.Sleep(5 * time.Second)
-	fmt.Println("Done goroutine")
-	//fmt.Println(time.Now())
-	res <- fmt.Sprintf("%s : In %s uploaded file %s\n", time.Now().Format(time.RFC3339), time.Since(start), file)
+	}
+	//time.Sleep(5 * time.Second)
+	//fmt.Println("Done goroutine")
+	if uploaded {
+		res <- fmt.Sprintf("%s : In %s uploaded file %s\n", time.Now().Format(time.RFC3339), time.Since(start), file)
+	} else {
+		res <- fmt.Sprintf("%s : failed to upload file %s\n", time.Now().Format(time.RFC3339), time.Since(start), file)
+	}
 	<-limit
-	//fmt.Srintf("%s : In %s uploaded file %s\n", time.Now(), time.Since(start), file)
-
 }

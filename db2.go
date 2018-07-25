c2aae55487daaf88da04f97c040df845d6e2e8ad
nirwander <nirwander@gmail.com>
2018-07-26 09:46:52 +1000

    db2

--- db2.go
+++ db2.go
@@ -6,14 +6,16 @@ import (
 	"os"
 	"time"
 
-	_ "github.com/mattn/go-oci8"
+	"gopkg.in/goracle.v2"
 )
 
 func main() {
 
+	//fmt.Println(2 << 19)
+	//panic("Stop")
 	os.Setenv("NLS_LANG", "")
 
-	db, err := sql.Open("oci8" /*os.Args[1]*/, `sys/sys@orcl?as=sysdba&prefetch_rows=1000`)
+	db, err := sql.Open("goracle" /*os.Args[1]*/, `sys/sys@orcl as sysdba`)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -30,22 +32,30 @@ func main() {
 	fmt.Printf("Successful 'as sysdba' connection. Large table records count: %v\n", cnt)
 	fmt.Printf("Count fetched in %s\n\n", time.Since(start))
 
-	objs := make([]int, 0, 100000)
+	type rec struct {
+		ObjID   int
+		ObjName string
+	}
+
+	//objs := make([]int, 0, 2<<19)
+	objs := make([]rec, 0, 2<<19)
 
 	start = time.Now()
-	rows, err := db.Query("select a_object_id from hr.large_table where rownum<=2000000")
+	rows, err := db.Query("select a_object_id, a_object_name from hr.large_table where rownum<=2000000", goracle.FetchRowCount(10000))
 	defer rows.Close()
 	for rows.Next() {
 		var obj int
-		err := rows.Scan(&obj)
+		var n string
+		err := rows.Scan(&obj, &n)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		objs = append(objs, obj)
+		objs = append(objs, rec{obj, n})
 	}
 
 	fmt.Println(len(objs))
 	fmt.Printf("Records fetched in %s\n", time.Since(start))
+	fmt.Println(objs[0])
 
 }
